24c872f64aa7600bd1a91ac997b6f07874a2a87d
zlstjq <zlstjq@gmail.com>
2018-12-10 01:43:34 +0900

    소방관 스토리끝

---
+++ project/mini.js
@@ -0,0 +1,150 @@
+$("#buu").on("click", function() {
+
+	var second = 20;
+	var flag = 0;
+
+	if (second == 20) {
+		$("#im2").css("visibility", "hidden");
+		$("#im3").css("visibility", "visible");
+	}
+
+	if (flag == 0) {
+		$("#im3").css("visibility", "visible");
+		$("#im4").css("visibility", "hidden");
+		$("#im3").css("margin-left", "22%");
+	}
+
+	if (flag == 1) {
+		$("#im4").css("visibility", "visible");
+		$("#im3").css("visibility", "hidden");
+		$("#im4").css("margin-left", "24%");
+	}
+
+	if (flag == 2) {
+		$("#im3").css("visibility", "visible");
+		$("#im4").css("visibility", "hidden");
+		$("#im3").css("margin-left", "25%");
+	}
+
+	if (flag == 3) {
+		$("#im4").css("visibility", "visible");
+		$("#im3").css("visibility", "hidden");
+		$("#im4").css("margin-left", "27%");
+	}
+
+	if (flag == 4) {
+		$("#im3").css("visibility", "visible");
+		$("#im4").css("visibility", "hidden");
+		$("#im3").css("margin-left", "28%");
+	}
+
+	if (flag == 5) {
+		$("#im4").css("visibility", "visible");
+		$("#im3").css("visibility", "hidden");
+		$("#im4").css("margin-left", "30%");
+	}
+
+	if (flag == 6) {
+		$("#im3").css("visibility", "visible");
+		$("#im4").css("visibility", "hidden");
+		$("#im3").css("margin-left", "31%");
+	}
+
+	if (flag == 7) {
+		$("#im4").css("visibility", "visible");
+		$("#im3").css("visibility", "hidden");
+		$("#im4").css("margin-left", "33%");
+	}
+
+	if (flag == 8) {
+		$("#im3").css("visibility", "visible");
+		$("#im4").css("visibility", "hidden");
+		$("#im3").css("margin-left", "34%");
+	}
+
+	if (flag == 9) {
+		$("#im4").css("visibility", "visible");
+		$("#im3").css("visibility", "hidden");
+		$("#im4").css("margin-left", "36%");
+	}
+
+	if (flag == 10) {
+		$("#im3").css("visibility", "visible");
+		$("#im4").css("visibility", "hidden");
+		$("#im3").css("margin-left", "37%");
+	}
+
+	if (flag == 11) {
+		$("#im4").css("visibility", "visible");
+		$("#im3").css("visibility", "hidden");
+		$("#im4").css("margin-left", "39%");
+	}
+
+	if (flag == 12) {
+		$("#im3").css("visibility", "visible");
+		$("#im4").css("visibility", "hidden");
+		$("#im3").css("margin-left", "40%");
+	}
+
+	if (flag == 13) {
+		$("#im4").css("visibility", "visible");
+		$("#im3").css("visibility", "hidden");
+		$("#im4").css("margin-left", "42%");
+	}
+
+	if (flag == 14) {
+		$("#im3").css("visibility", "visible");
+		$("#im4").css("visibility", "hidden");
+		$("#im3").css("margin-left", "43%");
+	}
+
+	if (flag == 15) {
+		$("#im4").css("visibility", "visible");
+		$("#im3").css("visibility", "hidden");
+		$("#im4").css("margin-left", "45%");
+	}
+
+	if (flag == 16) {
+		$("#im3").css("visibility", "visible");
+		$("#im4").css("visibility", "hidden");
+		$("#im3").css("margin-left", "46%");
+	}
+
+	if (flag == 17) {
+		$("#im4").css("visibility", "visible");
+		$("#im3").css("visibility", "hidden");
+		$("#im4").css("margin-left", "48%");
+	}
+
+	if (flag == 18) {
+		$("#im3").css("visibility", "visible");
+		$("#im4").css("visibility", "hidden");
+		$("#im3").css("margin-left", "49%");
+	}
+
+	if (flag == 19) {
+		$("#im4").css("visibility", "visible");
+		$("#im3").css("visibility", "hidden");
+		$("#im4").css("margin-left", "51%");
+	}
+
+	if (flag == 20) {
+		$("#im3").css("visibility", "visible");
+		$("#im4").css("visibility", "hidden");
+		$("#im3").css("margin-left", "52%");
+	}
+
+	if (flag == 21) {
+		$("#im4").css("visibility", "visible");
+		$("#im3").css("visibility", "hidden");
+		$("#im4").css("margin-left", "54%");
+	}
+
+	if (flag == 22) {
+		$("#im3").css("visibility", "visible");
+		$("#im4").css("visibility", "hidden");
+		$("#im3").css("margin-left", "55%");
+	}
+	flag++;
+
+});
